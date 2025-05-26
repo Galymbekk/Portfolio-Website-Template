7ff9715c36aecf51bf69b0d6b48bf56e215d74b8
Galymbekk <galymbek.batyrbekk@gmail.com>
2025-05-26 18:59:18 +0500

    one more commit

--- js/projects.js
+++ js/projects.js
@@ -1,122 +1,163 @@
 const projects = [
     {
-      title: "Portfolio Website",
+      title: "Жеке портфолио",
+      author:"Abdimutalip Ayan 24/7",
       link: "https://final-react-project-red.vercel.app",
       image: "images/project1.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Game Shop",
+      title: "Ойын дүкені",
+      author:"Tuzelbai Azamat 24/3",
       link: "https://azamattuz.github.io/Games---Shop",
       image: "images/project2.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Borcelle Fitness",
+      title: "Borcelle фитнес орталығы",
+      author:"Nurqadyr Baqbergen 24/5",
       link: "https://final-gym-project.netlify.app",
       image: "images/project3.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Personal Portfolio",
+      title: "Жеке портфолио",
+      author:"Berik Aziz 24/2",
       link: "https://portfolio-orcin-alpha-88.vercel.app",
       image: "images/project4.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "ProfScope",
+      title: "ProfScope мамандану орталығы",
+      author:"Unerbaev Imangali 24/7",
       link: "https://profscore-work.netlify.app",
       image: 'images/project5.png',
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Peaks of Almaty",
+      title: "Алматы шыңдары",
+      author:"Yerzhan Bagzhan 24/8",
       link: "https://final-react-project-xi.vercel.app",
       image: "images/project6.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Mini Games",
+      title: "Мини ойындар",
+      author:"Yesemuratov Asadbek 24/8",
       link: "https://final-react-nine.vercel.app",
       image: "images/project7.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Fitness Coach",
+      title: "Фитнес тренер",
+      author:"Yesbolat Tolganay 24/3",
       link: "https://silver-zabaione-d245cb.netlify.app/",
       image: "images/project8.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Library",
+      title: "Кітапхана",
+      author:"Jakan Mariyam 24/3",
       link: "https://aquamarine-tarsier-92df3c.netlify.app",
       image: "images/project9.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Galaxy/Planets",
+      title: "Галактика/Планеталар",
+      author:"Seisenbay Anuar 24/2",
       link: "https://oper08.github.io/Kosmos-Project/",
       image: "images/project10.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Nature",
+      title: "Табиғат",
+      author:"Abdikadirov Nurlan 25/2",
       link: "https://nurlan2913.github.io/boom-project/main.html",
       image: "images/project11.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
       title: "Chic Flowers",
+      author:"Zhaqanbay Ayala 24/6",
       link: "https://myjs-project.netlify.app",
       image: "images/project12.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-      title: "Weather App",
+      title: "Ауа-райы қосымшасы",
+      author:"Tabysbekuly Aibar 25/2",
       link: "https://aibart.github.io/Weather-App/fp.html",
       image: "images/project13.png",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-        title: "ToDo App",
+        title: "ToDo қосымшасы",
+        author:"Tugumbay Khadidzha 24/6",
         link: "https://final-projj.netlify.app/",
         image: "images/project14.png",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-        title: "Music",
+        title: "Музыка",
+        author:"Tugumbay Khadidzha 24/6",
         link: "https://drnyash.github.io/Qo",
         image: "images/project15.png",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-        title: "Movie Website",
+        title: "Кино сайты",
+        author:"Rakhymberdi Ayaulym 24/1",
         link: "https://project-movie1.netlify.app",
         image: "images/project16.png",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-        title: "Ancient Greece",
+        title: "Ежелгі Греция",
+        author:"Nurzhan Aziza 24/3",
         link: "https://visionary-squirrel-e11e5e.netlify.app",
         image: "images/project17.png",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-        title: "Animal Planet",
+        title: "Жануарлар Планетасы",
+        author:"Sharipkhan Danial 24/3",
         link: "https://noscam01.github.io/animal1/index.html",
         image: "images/project18.png",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
-        title: "Dorama Website",
+        title: "Дорама Веб-қосымшасы",
+        author:"Zhaqanbay Ayala 24/6",
         link: "https://doramaproject.netlify.app",
         image: "images/project19.png",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     },
     {
         title: "Drive Smart",
+        author:"Yesemuratov Asadbek 24/8",
         link: "https://asadbek96.github.io/Javascript-Project/",
         image: "images/project20.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
+    },
+    {
+        title: "Бюджет менеджері",
+        author:"Abdimutalip Ayan 24/7",
+        link: "https://menage-badget.vercel.app",
+        image: "images/project21.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
+    },
+    {
+        title: "Witcher",
+        author:"Asirbek Aibek 25/2",
+        link: "https://aibek5631.github.io/Witcher/witcher.html",
+        image: "images/project22.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
+    },
+    {
+        title: "WeHealth",
+        author:"Maratuly Azamat 24/6",
+        link: "https://wehealth.netlify.app",
+        image: "images/project23.png",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur fugiat..."
     }
   ];
@@ -149,6 +190,7 @@ function displayProjects(startIndex, endIndex) {
           </a>
           <div class="text mt-3 float-right d-block">
             <h3 class="heading"><a href="${project.link}">${project.title}</a></h3>
+            <h6 class="author-title">${project.author}</h6>
             <p>${project.description}</p>
           </div>
         </div>
